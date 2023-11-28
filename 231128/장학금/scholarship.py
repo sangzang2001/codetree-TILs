fd5216097dcc956ef5ea8288185b9e857fa5f2cd
sangzang2001 <75602987+sangzang2001@users.noreply.github.com>
2023-11-28 21:18:30 +0900

    Title: 장학금, Time: 181ms, Memory: 24MB, Status: Passed - CodeTree

--- 231128/장학금/scholarship.py
+++ 231128/장학금/scholarship.py
@@ -2,8 +2,8 @@ inp=input().split()
 mid, last=int(inp[0]), int(inp[1])
 
 if mid>=90 and last>=95:
-    print(10)
+    print(100000)
 elif mid>=90 and last>=90:
-    print(5)
+    print(50000)
 else:
     print(0)
