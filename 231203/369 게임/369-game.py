1a569fea6fb2935c18aa692aedda31f538d91ec7
sangzang2001 <75602987+sangzang2001@users.noreply.github.com>
2023-12-03 21:37:49 +0900

    Title: 369 게임, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 231203/369 게임/369-game.py
+++ 231203/369 게임/369-game.py
@@ -1,7 +1,7 @@
 n=int(input())
 
 for i in range(1, n+1, 1):
-    if i%3==0 or i%10==3 or i%10==6 or i%10==9:
+    if i%3==0 or i%10==3 or i%10==6 or i%10==9 or i>=30 and i<40 or i>=60 and i<70 or i>=90 and i<100:
         print(0, end=' ')
     else:
         print(i, end=' ')
